b9d6ec76291154a38e2c46bcfe0fabee5114d43d
gnanod <1995gnanod@gmail.com>
2020-10-07 12:56:44 +0530

    Edit Business package

--- TicketingSystemBackEnd/src/main/java/csse/business/custom/Impl/UserBOImpl.java
+++ TicketingSystemBackEnd/src/main/java/csse/business/custom/Impl/UserBOImpl.java
@@ -1,9 +1,23 @@
 package csse.business.custom.Impl;
 
 import csse.business.custom.UserBO;
+import csse.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserBOImpl implements UserBO {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public boolean userLogin(String id, String password) {
+        String userId = userRepository.userLogin(id, password);
+        boolean result = true;
+        if (userId == null) {
+            result = false;
+        }
+        return result;
+    }
 }
