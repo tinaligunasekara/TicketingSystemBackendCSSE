4c13fc9a85eb3654f81b4aeffcd8feba4ddfd657
gnanod <1995gnanod@gmail.com>
2020-10-14 13:20:06 +0530

    handle Transaction

--- TicketingSystemBackEnd/src/main/java/csse/business/custom/impl/ForeignUserBOImpl.java
+++ TicketingSystemBackEnd/src/main/java/csse/business/custom/impl/ForeignUserBOImpl.java
@@ -5,6 +5,7 @@ import csse.entity.User;
 import csse.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class ForeignUserBOImpl implements ForeignUserBO {
@@ -12,6 +13,7 @@ public class ForeignUserBOImpl implements ForeignUserBO {
     @Autowired
     private UserRepository userRepository;
 
+    @Transactional
     @Override
     public User addForeignUser(User user) {
         return userRepository.save(user);
--- TicketingSystemBackEnd/src/main/java/csse/business/custom/impl/LocalUserBOImpl.java
+++ TicketingSystemBackEnd/src/main/java/csse/business/custom/impl/LocalUserBOImpl.java
@@ -5,6 +5,7 @@ import csse.entity.User;
 import csse.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,6 +15,7 @@ public class LocalUserBOImpl implements LocalUserBO {
     @Autowired
     private UserRepository userRepository;
 
+    @Transactional
     @Override
     public User addLocalUser(User user) {
         return userRepository.save(user);
